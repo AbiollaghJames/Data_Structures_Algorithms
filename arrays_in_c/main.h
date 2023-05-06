0bef15c8f7b73473e2ecb04d4f62864af5b7a1f2
AbiollaghJames <abiollajames@protonmail.com>
2023-05-06 01:46:12 -0700

    Header

--- arrays_in_c/main.h
+++ arrays_in_c/main.h
@@ -7,5 +7,10 @@
 
 void print_array(int *arr, int size);
 void reverse_array(int arr[], int size);
+void search_element(int arr[], int size, int key);
+void insert_at_position(int arr[], int size, int num, int pos);
+void insert_at_beginning(int arr[], int size, int num);
+void insert_at_end(int arr[], int size, int num);
+void delete_at_pos(int arr[], int size, int pos);
 
 #endif
