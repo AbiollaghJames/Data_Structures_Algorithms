a7fab4d5368a29dfe865c1fc733f455666d6a1c4
AbiollaghJames <abiollajames@protonmail.com>
2023-05-06 01:16:17 -0700

    Main file for Tests

--- arrays_in_c/main_test.c
+++ arrays_in_c/main_test.c
@@ -3,20 +3,18 @@
 
 int main(void)
 {
-	int array[5];
 
-	array[0] = 98;
-	array[1] = 402;
-	array[2] = -198;
-	array[3] = 298;
-	array[4] = -1024;
+	int a[15] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	print_array(array, 5);
-
-	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
-
-	print_array(a, sizeof(a) / sizeof(int));
+	search_element(a, sizeof(a) / sizeof(int), 98);
 	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
+	/*for this to work, arr[size] has to be decalared otherwise you loose the last value */
+	insert_at_position(a, sizeof(a) / sizeof(int), 200, 5);
+	print_array(a, sizeof(a) / sizeof(int));
+	insert_at_beginning(a, sizeof(a) / sizeof(int), 350);
+	print_array(a, sizeof(a) / sizeof(int));	
+	insert_at_end(a, sizeof(a) / sizeof(int), 676);
 	print_array(a, sizeof(a) / sizeof(int));
 
 	return (0);
